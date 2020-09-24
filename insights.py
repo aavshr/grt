69667e23ff86318b9a5433f15f2f77d7be5c47e7
aavshr <aavash09@gmail.com>
2020-09-24 18:29:45 +0200

    add comments

--- insights.py
+++ insights.py
@@ -24,6 +24,7 @@ class Chart:
             "week": 60*60*24*7,
         }
 
+    # get submitted reviews bucketed by preiods based on duration 
     def __get_insights(self, duration:str, period:str):
         if not self.__durations[duration] or not self.__periods[period]:
             raise ValueError("bad duration or period")
@@ -32,11 +33,12 @@ class Chart:
         submitted_reviews = rev_req_store.get(since)
         return self.__bucket_submissions(since, period, submitted_reviews)
        
-           
+    # convert duration into iso 8601 date format
     def __get_since(self, days:int):
         since = datetime.now() - timedelta(days=days)
         return since.isoformat()
-    
+
+    # bucket submitted reviews based on submission timestamp since date averaged by period
     def __bucket_submissions(self, since:str, period:str, submitted_reviews:list): 
         now_posix = int(datetime.now().timestamp())
         since_posix = int(isoparse(since).timestamp())
@@ -44,26 +46,34 @@ class Chart:
         buckets = {}
         average_buckets = {}
         separators = []
+        
+        # separators are calculated based on period
+        # for eg. if period is "day", separators are distanced by 86400 seconds
         start = since_posix+self.__periods[period]
         for start in range(since_posix, now_posix+1, self.__periods[period]):
             buckets[start] = []
             separators.append(start)
 
+        # fill the buckets
         for rev in submitted_reviews:
             for separator in separators:
+                # the separaotrs are sorted in increasing order
+                # so a simple comparision suffices here
                 if separator > rev["requested_at"]:
                     buckets[separator].append(rev["crt"])
                     break
                 
+        # compute average for each bucket
         for separator in buckets:
             date = datetime.fromtimestamp(separator)
             crts = buckets[separator]
-            average_buckets[date] = nan
+            average_buckets[date] = nan # nan here to denote missing data for the chart
             if len(crts) != 0:
                 average_buckets[date] = round(mean(buckets[separator])/60, 2)
             
         return average_buckets
 
+    # generate html chart with bokeh
     def __generate_chart(self, buckets:dict):
         p = figure(title="Average code review turnarounds", 
                 x_axis_type="datetime",
@@ -78,10 +88,12 @@ class Chart:
         p.line(x, y, color="red", legend_label="moving average code review turnaround")
         return file_html(p, CDN, "Average code review turnarounds")
 
+    # get html chart
     def get_chart(self, duration:str, period:str):
         buckets = self.__get_insights(duration, period)
         return self.__generate_chart(buckets)
     
+    # get json of average values
     def get_json(self, duration:str, period:str):
         buckets = self.__get_insights(duration, period)
         for date in buckets:

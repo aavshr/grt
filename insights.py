d093e232ad438294de051ed3fd7a7b9565f5a0f8
aavshr <aavash09@gmail.com>
2020-09-25 12:10:00 +0200

    format with black

--- insights.py
+++ insights.py
@@ -10,47 +10,48 @@ from bokeh.models import HoverTool
 
 from reviews import rev_req_store
 
+
 class Chart:
     def __init__(self):
         # maps durations to number of days
         self.__durations = {
-            "week": 7, #number of days
-            "month": 30, #number of days
+            "week": 7,  # number of days
+            "month": 30,  # number of days
         }
 
         # maps periods to number of seconds
         self.__periods = {
-            "day":60*60*24, 
-            "week": 60*60*24*7,
+            "day": 60 * 60 * 24,
+            "week": 60 * 60 * 24 * 7,
         }
 
-    # get submitted reviews bucketed by preiods based on duration 
-    def __get_insights(self, duration:str, period:str):
+    # get submitted reviews bucketed by preiods based on duration
+    def __get_insights(self, duration: str, period: str):
         if not self.__durations[duration] or not self.__periods[period]:
             raise ValueError("bad duration or period")
-    
+
         since = self.__get_since(self.__durations[duration])
         submitted_reviews = rev_req_store.get(since)
         return self.__bucket_submissions(since, period, submitted_reviews)
-       
+
     # convert duration into iso 8601 date format
-    def __get_since(self, days:int):
+    def __get_since(self, days: int):
         since = datetime.now() - timedelta(days=days)
         return since.isoformat()
 
     # bucket submitted reviews based on submission timestamp since date averaged by period
-    def __bucket_submissions(self, since:str, period:str, submitted_reviews:list): 
+    def __bucket_submissions(self, since: str, period: str, submitted_reviews: list):
         now_posix = int(datetime.now().timestamp())
         since_posix = int(isoparse(since).timestamp())
-        
+
         buckets = {}
         average_buckets = {}
         separators = []
-        
+
         # separators are calculated based on period
         # for eg. if period is "day", separators are distanced by 86400 seconds
-        start = since_posix+self.__periods[period]
-        for start in range(since_posix, now_posix+1, self.__periods[period]):
+        start = since_posix + self.__periods[period]
+        for start in range(since_posix, now_posix + 1, self.__periods[period]):
             buckets[start] = []
             separators.append(start)
 
@@ -62,26 +63,27 @@ class Chart:
                 if separator > rev["requested_at"]:
                     buckets[separator].append(rev["crt"])
                     break
-                
+
         # compute average for each bucket
         for separator in buckets:
             date = datetime.fromtimestamp(separator)
             crts = buckets[separator]
-            average_buckets[date] = nan # nan here to denote missing data for the chart
+            average_buckets[date] = nan  # nan here to denote missing data for the chart
             if len(crts) != 0:
-                average_buckets[date] = round(mean(buckets[separator])/60, 2)
-            
+                average_buckets[date] = round(mean(buckets[separator]) / 60, 2)
+
         return average_buckets
 
     # generate html chart with bokeh
-    def __generate_chart(self, buckets:dict):
-        p = figure(title="Average code review turnarounds", 
-                x_axis_type="datetime",
-                x_axis_label="date",
-                y_axis_label="average turnaround (mins)",
-                plot_height=800,
-                plot_width=800,
-            )
+    def __generate_chart(self, buckets: dict):
+        p = figure(
+            title="Average code review turnarounds",
+            x_axis_type="datetime",
+            x_axis_label="date",
+            y_axis_label="average turnaround (mins)",
+            plot_height=800,
+            plot_width=800,
+        )
         x = list(buckets.keys())
         y = list(buckets.values())
         p.scatter(x, y, color="red")
@@ -89,13 +91,14 @@ class Chart:
         return file_html(p, CDN, "Average code review turnarounds")
 
     # get html chart
-    def get_chart(self, duration:str, period:str):
+    def get_chart(self, duration: str, period: str):
         buckets = self.__get_insights(duration, period)
         return self.__generate_chart(buckets)
-    
+
     # get json of average values
-    def get_json(self, duration:str, period:str):
+    def get_json(self, duration: str, period: str):
         buckets = self.__get_insights(duration, period)
         for date in buckets:
-            if isnan(buckets[date]): buckets[date] = 0 
+            if isnan(buckets[date]):
+                buckets[date] = 0
         return buckets
--- reviews.py
+++ reviews.py
@@ -18,15 +18,15 @@ from deta import Deta
 class ReviewRequestStore:
     def __init__(self):
         self.db = Deta().Base("code_reviews")
-    
+
     # get review req from pull request number and reviewer
-    def __get_review_req(self, pr_num:int, reviewer:str):
+    def __get_review_req(self, pr_num: int, reviewer: str):
         # generator
-        review_reqs_gen = next(self.db.fetch({
-            "submitted": False,
-            "pull_request": pr_num,
-            "reviewer": reviewer
-        }))
+        review_reqs_gen = next(
+            self.db.fetch(
+                {"submitted": False, "pull_request": pr_num, "reviewer": reviewer}
+            )
+        )
 
         review_reqs = []
         for r in review_reqs_gen:
@@ -36,63 +36,65 @@ class ReviewRequestStore:
         if len(review_reqs) == 0:
             raise Exception("No corresponding review request found")
 
-        if len(review_reqs) > 1: 
-            raise Exception("Found multiple imcomplete reviews for same pull request and reviewer")
+        if len(review_reqs) > 1:
+            raise Exception(
+                "Found multiple imcomplete reviews for same pull request and reviewer"
+            )
 
         return review_reqs[0]
 
     # store review request
-    def store(self, payload:dict):
+    def store(self, payload: dict):
         # POSIX timestamp
         current_time = int(datetime.now(timezone.utc).timestamp())
         item = {
             "reviewer": payload["requested_reviewer"]["login"],
             "pull_request": payload["pull_request"]["number"],
             "requested_at": current_time,
-            "submitted": False
+            "submitted": False,
         }
 
         self.db.put(item)
 
     # mark review request complete
-    def mark_complete(self, payload:dict):
+    def mark_complete(self, payload: dict):
         submission_time = int(isoparse(payload["review"]["submitted_at"]).timestamp())
 
         pr_num = payload["pull_request"]["number"]
         reviewer = payload["review"]["user"]["login"]
         review_req = self.__get_review_req(pr_num, reviewer)
 
-        # updates to the review request 
+        # updates to the review request
         updates = {
             "submitted": True,
             "submitted_at": submission_time,
-            "crt": submission_time-review_req["requested_at"]
-        } 
+            "crt": submission_time - review_req["requested_at"],
+        }
 
-        self.db.update(updates, review_req["key"]) 
+        self.db.update(updates, review_req["key"])
         return
 
     # delete review request
-    def delete(self, payload:dict):
+    def delete(self, payload: dict):
         pr_num = payload["pull_request"]["number"]
         reviewer = payload["requested_reviewer"]["login"]
 
         review_req = self.__get_review_req(pr_num, reviewer)
         self.db.delete(review_req["key"])
-    
+
     # get review requests created since date
-    def get(self, created_since:str):
+    def get(self, created_since: str):
         since = int(isoparse(created_since).timestamp())
 
-        review_reqs_since_gen = next(self.db.fetch({
-            "requested_at?gte": since,
-            "submitted": True
-        }))
+        review_reqs_since_gen = next(
+            self.db.fetch({"requested_at?gte": since, "submitted": True})
+        )
 
         review_reqs_since = []
         for req in review_reqs_since_gen:
             review_reqs_since.append(req)
 
         return review_reqs_since
+
 
 rev_req_store = ReviewRequestStore()
